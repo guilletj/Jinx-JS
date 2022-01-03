def86982fc7ac53736b23ac005cc6d2597c4d038
Edyone <73330380+guilletj@users.noreply.github.com>
2022-01-03 17:42:24 +0100

    Added more logs so we know how's the loading proccess going.

--- jinx.js
+++ jinx.js
@@ -12,7 +12,9 @@ try {
     process.exit(1);
 }
 
+console.log('Loading modules...')
 const interactionCreate = require('./modules/interactionCreate.js');
+console.log('All modules loaded!')
 
 const allIntents = new Intents();
 allIntents.add(Intents.FLAGS.DIRECT_MESSAGES,
@@ -31,22 +33,24 @@ allIntents.add(Intents.FLAGS.DIRECT_MESSAGES,
     Intents.FLAGS.GUILD_VOICE_STATES,
     Intents.FLAGS.GUILD_WEBHOOKS);
 
+console.log('Initializing client and commands...')
 const client = new Client({ intents: allIntents });
-
 client.commands = new Collection();
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.data.name, command);
 }
+console.log('Everything fine for now... Logging to Discord...')
 
 client.login(cfg.token).then(() => {
-    console.log('Ready');
+    console.log('Logging success! Everything is ready now');
     client.user.setStatus(cfg.profile.presence.status)
     client.user.setActivity(cfg.profile.presence.text, {type: cfg.profile.presence.type});
 });
 
 client.on('interactionCreate', (interaction) => {
-    interactionCreate(interaction, client);
+    interactionCreate(interaction, client).then();
 });
